5b81aa719335fd492c18dd7ada8bb158222ce4d8
Unknown <jasper_thecrown@hotmail.com>
2019-02-20 12:15:02 +0100

    Setup files

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="Landing">
+        <h1>App</h1>
+      </div>
+    );
+  }
+}
+
+export default Landing;
