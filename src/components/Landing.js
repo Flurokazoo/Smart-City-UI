dc3bd923344ca4da28083abd8fe1ea779a0e579e
Unknown <jasper_thecrown@hotmail.com>
2019-02-20 14:33:02 +0100

    Add cards for landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
+import './Landing.css'
+
 
 class Landing extends Component {
-  render() {
-    return (
-      <div className="Landing">
-        <h1>App</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Landing">
+                <div className="container">
+                    <div className="row">
+                        <h1 className="display-3">Welcome to the Smart Parking UI</h1>
+                    </div>
+                    <div className="row">
+                        <p className="lead">This is the visual representation of the Smart Parking API. It will allow you to see the various available methods visually.</p>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm">
+                            <div className="card text-center">
+                                <div className="card-body">
+                                    <h3 className="card-title">Sector overview</h3>
+                                    <i className="fas fa-car-side fa-10x"></i>
+                                    <p className="card-text">An overview of all sectors</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm">
+                            <div className="card text-center">
+                                <div className="card-body">
+                                    <h3 className="card-title">Distance overview</h3>
+                                    <i className="fas fa-route fa-10x"></i>
+                                    <p className="card-text">An overview of all sectors</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm">
+                            <div className="card text-center">
+                                <div className="card-body">
+                                    <h3 className="card-title">Grid overview</h3>
+                                    <i className="fas fa-th fa-10x"></i>
+                                    <p className="card-text">An overview of all sectors</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default Landing;
