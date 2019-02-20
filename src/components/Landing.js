849788f7d481c67434f3dc3ec97653f5459bce72
Unknown <jasper_thecrown@hotmail.com>
2019-02-20 15:22:30 +0100

    Add classes for landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './Landing.css'
 
-
 class Landing extends Component {
     render() {
         return (
@@ -13,35 +12,37 @@ class Landing extends Component {
                     <div className="row">
                         <p className="lead">This is the visual representation of the Smart Parking API. It will allow you to see the various available methods visually.</p>
                     </div>
-                    <div className="row">
-                        <div className="col-sm">
-                            <div className="card text-center">
-                                <div className="card-body">
-                                    <h3 className="card-title">Sector overview</h3>
-                                    <i className="fas fa-car-side fa-10x"></i>
-                                    <p className="card-text">An overview of all sectors</p>
+                    <div className="cards">
+                        <div className="row">
+                            <div className="col-sm">
+                                <div className="card text-center">
+                                    <div className="card-body">
+                                        <h3 className="card-title">Sector overview</h3>
+                                        <i className="fas fa-car-side fa-10x"></i>
+                                        <p className="card-text">An overview of all sectors</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-sm">
-                            <div className="card text-center">
-                                <div className="card-body">
-                                    <h3 className="card-title">Distance overview</h3>
-                                    <i className="fas fa-route fa-10x"></i>
-                                    <p className="card-text">An overview of all sectors</p>
+                            <div className="col-sm">
+                                <div className="card text-center">
+                                    <div className="card-body">
+                                        <h3 className="card-title">Distance overview</h3>
+                                        <i className="fas fa-route fa-10x"></i>
+                                        <p className="card-text">An overview of all sectors</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-sm">
-                            <div className="card text-center">
-                                <div className="card-body">
-                                    <h3 className="card-title">Grid overview</h3>
-                                    <i className="fas fa-th fa-10x"></i>
-                                    <p className="card-text">An overview of all sectors</p>
+                            <div className="col-sm">
+                                <div className="card text-center">
+                                    <div className="card-body">
+                                        <h3 className="card-title">Grid overview</h3>
+                                        <i className="fas fa-th fa-10x"></i>
+                                        <p className="card-text">An overview of all sectors</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
+                        </div>
                     </div>
 
                 </div>
