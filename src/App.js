5b076cb6cf6245c7d9730a6bbe55bef0e555fc3e
Unknown <jasper_thecrown@hotmail.com>
2019-02-20 13:14:30 +0100

    Add Bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Landing from './components/Landing';
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 class App extends Component {
   render() {
