7878171e2900271fb5114b5d33b47bf988311e7a
Unknown <jasper_thecrown@hotmail.com>
2019-02-26 14:14:54 +0100

    Add loader

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -7,22 +7,26 @@ export class MapContainer extends Component {
 
     constructor(props) {
         super(props);
-        console.log(props);
         this.state = {
-            coordinates: []
+            coordinates: [{}],
+            loading: 'initial'
         };
+
     }
     componentDidMount() {
+        this.setState({
+            loading: 'true'
+        })
 
         axios.get('https://smartcity-parking-api.herokuapp.com/sectors')
             .then(res => {
                 const sectors = res.data.data
+                let coordinates = []
                 sectors.map((sector, i) => {
                     axios.get(sector.self_links.detail)
                         .then(res => {
-                            console.log(res.data.data.sector_data.sector_id)
                             const coordinateArray = res.data.data.coordinates
-                            this.state.coordinates[i] = {
+                            coordinates[i] = {
                                 latlng: [],
                                 id: res.data.data.sector_data.sector_id
                             }
@@ -31,22 +35,24 @@ export class MapContainer extends Component {
                                     lat: coordinate.latitude,
                                     lng: coordinate.longitude
                                 }
-                                console.log(i)
-
-                                this.state.coordinates[i].latlng.push(latlng)
+                                coordinates[i].latlng.push(latlng)
                             })
-                            // .then(res =>{
-                            //     console.log(res)
-                            // })
+
+                            coordinates[i].latlng.push({
+                                lat: coordinateArray[0].latitude,
+                                lng: coordinateArray[0].longitude
+                            })
+
                         })
                 })
-
+                this.setState({
+                    coordinates: coordinates,
+                    loading: 'false'
+                })
             })
     }
     render() {
 
-        const coordinates = this.state.coordinates
-        console.log(Object.values(this.state.coordinates[0]))
 
         const triangleCoords = [
             { lat: 25.774, lng: -80.190 },
@@ -55,45 +61,53 @@ export class MapContainer extends Component {
             { lat: 25.774, lng: -80.190 }
         ];
 
-        console.log(coordinates)
-        let polygons = <Polygon
-            paths={triangleCoords}
-            strokeColor="#0000FF"
-            strokeOpacity={0.8}
-            strokeWeight={2}
-            fillColor="#0000FF"
-            fillOpacity={0.35} />
+        if (this.state.loading === 'initial') {
+            console.log('This happens 2nd - after the class is constructed. You will not see this element because React is still computing changes to the DOM.');
+            return <h2>Intializing...</h2>;
+        }
 
-        return (
-            <Map google={this.props.google}
-                style={{ width: '100%', height: '100%', position: 'relative' }}
-                className={'map'}
-                initialCenter={{
-                    lat: 51.9120937,
-                    lng: 4.534785
-                }}
-                zoom={14}>
-                {this.state.coordinates.map(coordinate => {
-                    console.log(coordinate)
-                    return (
+
+        if (this.state.loading === 'true') {
+            console.log('This happens 5th - when waiting for data.');
+            return <h2>Loading...</h2>;
+        }
+
+
+        if(this.state.loading === 'false') {
+            const coordinates = this.state.coordinates
+
+            return (
+                <Map google={this.props.google}
+                    style={{ width: '100%', height: '100%', position: 'relative' }}
+                    className={'map'}
+                    initialCenter={{
+                        lat: 51.9120937,
+                        lng: 4.534785
+                    }}
+                    zoom={14}>
+                    {/* {coordinates.map(coordinate => {
+                        return (
+                            <Polygon
+                                paths={coordinate.latlng}
+                                strokeColor="#0000FF"
+                                strokeOpacity={0.8}
+                                strokeWeight={2}
+                                fillColor="#0000FF"
+                                fillOpacity={0.35} />
+                        )
+                    })} */}
                     <Polygon
-                    paths={coordinate.latlng}
-                    strokeColor="#0000FF"
-                    strokeOpacity={0.8}
-                    strokeWeight={2}
-                    fillColor="#0000FF"
-                    fillOpacity={0.35} />
-                    )
-                })}
-                <Polygon
-                    paths={triangleCoords}
-                    strokeColor="#0000FF"
-                    strokeOpacity={0.8}
-                    strokeWeight={2}
-                    fillColor="#0000FF"
-                    fillOpacity={0.35} />
-            </Map>
-        );
+                        paths={triangleCoords}
+                        strokeColor="#0000FF"
+                        strokeOpacity={0.8}
+                        strokeWeight={2}
+                        fillColor="#0000FF"
+                        fillOpacity={0.35} />
+                </Map>
+            );
+
+        }
+        
     }
 }
 
