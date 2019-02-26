35ecb75a2478376461740b2e6bcfefced7f2ad41
Unknown <jasper_thecrown@hotmail.com>
2019-02-26 16:51:28 +0100

    Implement correct data binding

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper, Polygon } from 'google-maps-react';
 import axios from 'axios';
 
-
 export class MapContainer extends Component {
 
     constructor(props) {
@@ -13,50 +12,52 @@ export class MapContainer extends Component {
         };
 
     }
+
     componentDidMount() {
         this.setState({
             loading: 'true'
         })
-
-        axios.get('https://smartcity-parking-api.herokuapp.com/sectors')
-            .then(res => {
-                const sectors = res.data.data
-                let coordinates = []
-                sectors.map((sector, i) => {
-                    axios.get(sector.self_links.detail)
-                        .then(res => {
-                            const coordinateArray = res.data.data.coordinates
-                            coordinates[i] = {
-                                latlng: [],
-                                id: res.data.data.sector_data.sector_id
-                            }
-                            coordinateArray.map(coordinate => {
-                                let latlng = {
-                                    lat: coordinate.latitude,
-                                    lng: coordinate.longitude
-                                }
-                                coordinates[i].latlng.push(latlng)
-                            })
-
-                            coordinates[i].latlng.push({
-                                lat: coordinateArray[0].latitude,
-                                lng: coordinateArray[0].longitude
-                            })
-
-                        })
-                })
-                this.setState({
-                    coordinates: coordinates,
-                    loading: 'false'
-                })
-            })
+        this.getData()
     }
+    
+    getData = async () => {
+        let coordinates = []
+        let res = await axios.get('https://smartcity-parking-api.herokuapp.com/sectors')
+        const sectors = await res.data.data
+
+        const sectorsLinks = await Promise.all(sectors.map(sector => axios.get(sector.self_links.detail)))
+
+        sectorsLinks.map((sector, i) => {
+            const coordinateArray = sector.data.data.coordinates;
+            coordinates[i] = {
+                latlng: [],
+                id: sector.data.data.sector_data.sector_id
+            }
+            coordinateArray.map(coordinate => {
+                let latlng = {
+                    lat: parseFloat(coordinate.latitude),
+                    lng: parseFloat(coordinate.longitude)
+                }
+                coordinates[i].latlng.push(latlng)
+            })
+    
+            coordinates[i].latlng.push({
+                lat: parseFloat(coordinateArray[0].latitude),
+                lng: parseFloat(coordinateArray[0].longitude)
+            })
+        });
+        console.log(coordinates);
+        this.setState({
+            coordinates: coordinates,
+            loading: 'false'
+        })
+    }
+
+   
+    
     render() {
 
         const { coordinates, loading } = this.state
-        let array = ['kaas,', 'worst']
-        console.log(array)
-
         const triangleCoords = [
             { lat: 25.774, lng: -80.190 },
             { lat: 18.466, lng: -66.118 },
@@ -65,19 +66,17 @@ export class MapContainer extends Component {
         ];
 
         if (loading === 'initial') {
-            console.log('This happens 2nd - after the class is constructed. You will not see this element because React is still computing changes to the DOM.');
             return <h2>Intializing...</h2>;
         }
 
 
         if (loading === 'true') {
-            console.log('This happens 5th - when waiting for data.');
             return <h2>Loading...</h2>;
         }
 
 
-        if(loading === 'false') {
-            console.log(coordinates)
+        if(loading === 'false' && this.state.coordinates) {
+            console.log(coordinates[0].latlng)
             return (
                 <Map google={this.props.google}
                     style={{ width: '100%', height: '100%', position: 'relative' }}
@@ -87,7 +86,7 @@ export class MapContainer extends Component {
                         lng: 4.534785
                     }}
                     zoom={14}>
-                    {/* {coordinates.map(coordinate => {
+                    {coordinates.map(coordinate => {
                         return (
                             <Polygon
                                 paths={coordinate.latlng}
@@ -97,7 +96,7 @@ export class MapContainer extends Component {
                                 fillColor="#0000FF"
                                 fillOpacity={0.35} />
                         )
-                    })} */}
+                    })}
                     <Polygon
                         paths={triangleCoords}
                         strokeColor="#0000FF"
