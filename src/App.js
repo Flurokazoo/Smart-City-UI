7c03dd7413f2ff77fbfd9cf80be335b6105fa3a5
Unknown <jasper_thecrown@hotmail.com>
2019-03-07 13:42:18 +0100

    Add sectors.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import 'bulma/css/bulma.css'
 import Landing from './components/Landing'
 import Notfound from './components/Notfound'
+import Sectors from './components/Sectors'
 
 
 class App extends Component {
@@ -11,7 +12,8 @@ class App extends Component {
     return (
       <div className="App">
         <Switch>
-          <Route exact path="/" component={Landing} />         
+          <Route exact path="/" component={Landing} />
+          <Route path="/sectors" component={Sectors} />
           <Route component={Notfound} />
         </Switch>    
       </div>
---
+++ src/components/Sectors.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Sectors = (props) =>
+    <div>Hello</div>
+
+export default Sectors
