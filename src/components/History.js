380eb0fffffe18beff5beadb3fd3a16b5fb897b8
Unknown <jasper_thecrown@hotmail.com>
2019-03-22 11:21:28 +0100

    Change 'history' to class component

--- src/components/History.js
+++ src/components/History.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Navbar from './Navbar'
 
-const History = (props) => {
-    return <div className='page'>
-        <Navbar />
-        <div className="content">
-            <section className="section">
-                <p className="is-size-4">History</p>
-            </section>
+class History extends Component {
+    render() {
+        return <div className='page'>
+            <Navbar />
+            <div className="content">
+                <section className="section">
+                    <p className="is-size-4">History</p>
+                </section>
+            </div>
         </div>
-    </div>
-
+    }
 }
+
+
 
 
 export default History
