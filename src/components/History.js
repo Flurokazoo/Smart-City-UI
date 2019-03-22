c6f796729342b2eb9fd55cb87f7682b09e4afe63
Unknown <jasper_thecrown@hotmail.com>
2019-03-22 11:58:45 +0100

    Add basic structure for 'history'

--- src/components/History.js
+++ src/components/History.js
@@ -1,14 +1,69 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
 
 class History extends Component {
+    state = {
+        sectorData: null,
+        value: null,
+    }
+
+    componentDidMount() {
+        this.getData()
+    }
+
+    getData = async () => {
+        let res = await axios.get('https://smartcity-parking-api.herokuapp.com/sectors')
+        const sectors = await res.data.data
+        const sectorsLinks = await Promise.all(sectors.map(sector => axios.get(sector.self_links.history)))
+
+        console.log(sectorsLinks)
+
+        this.setState({
+            sectorData: sectors
+        })
+    }
+
     render() {
+        const { sectorData, value } = this.state
+        let content, loadedContent
+
+        if (sectorData != null) {
+            content =
+                <div className="columns">
+                    <div className="column">
+                        <h3>Choose a sector:</h3>
+
+                        <div className="columns is-multiline">
+                            {sectorData.map((sector, i) => {
+                                let id = sector.sector_id
+                                return <div class="column is-1">
+                                    <button onClick={this.handleClick} value={id} class="button is-large is-success">{id}</button>
+                                </div>
+                            })}
+                        </div>
+                    </div>
+                    <div className="column">
+                    </div>
+                </div>
+
+        } else {
+            content = <a class="button is-loading is-large">Content is loading</a>
+        }
+
         return <div className='page'>
             <Navbar />
-            <div className="content">
-                <section className="section">
-                    <p className="is-size-4">History</p>
-                </section>
+            <div className="container">
+                <div className="content">
+                    <section className="section">
+                        <h1 className="title is-size-1">Single sector</h1>
+                        {content}
+
+                        <hr />
+
+                        {loadedContent}
+                    </section>
+                </div>
             </div>
         </div>
     }
