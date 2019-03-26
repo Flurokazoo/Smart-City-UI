a621991ba4b3a26328a541298d582e486747d00c
Unknown <jasper_thecrown@hotmail.com>
2019-03-26 16:50:32 +0100

    Add Chart logic for history

--- src/components/History.js
+++ src/components/History.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-import Navbar from './Navbar'
+import Herosub from './Herosub'
 import axios from 'axios'
 import DateRangePicker from '@wojtekmaj/react-daterange-picker'
+import moment from 'moment'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 class History extends Component {
     state = {
@@ -9,7 +11,9 @@ class History extends Component {
         url: null,
         value: null,
         dates: [new Date(), new Date()],
-        set: false
+        set: false,
+        history: [],
+        historyLoaded: false
     }
 
     componentDidMount() {
@@ -30,13 +34,52 @@ class History extends Component {
     }
 
     onChange = dates => {
+        console.log(dates)
         this.setState({
             dates: dates,
             set: true
+        }, () => {
+            this.getHistory();
         })
+
+
     }
 
     getHistory = async () => {
+        const { url, dates } = this.state
+        let { history } = this.state
+        const day = 86400
+
+        console.log(moment(dates[1]).unix() - moment(dates[0]).unix())
+        let res = await axios.get(url, {
+            params: {
+                start: moment(dates[0]).unix(),
+                end: moment(dates[1]).unix(),
+                limit: 100000
+            }
+        })
+        await res.data.data.entries.map((entry) => {
+            console.log(entry)
+            history.push({
+                'name': moment.unix(entry.timestamp).format(),
+                'occupance': entry.average_occupance * 100
+            })
+        })
+        this.setState({
+            history: history
+        })
+        if (res.data.pagination.next_url) {
+            this.setState({
+                url: res.data.pagination.next_url,
+                historyLoaded: false
+            })
+            this.getHistory()
+        } else {
+            this.setState({
+                historyLoaded: true
+            })
+        }
+
 
     }
 
@@ -53,20 +96,20 @@ class History extends Component {
     }
 
     render() {
-        const { sectorData, url, value } = this.state
-        let content, loadedContent
+        const { sectorData, value, set, history, historyLoaded } = this.state
+        let content, loadedContent, setContent
 
         if (sectorData != null) {
             content =
                 <div className="columns">
                     <div className="column">
-                        <h3>Choose a sector:</h3>
+                        <h1 class="title is-1">Choose a sector below:</h1>
 
                         <div className="columns is-multiline">
                             {sectorData.map((sector, i) => {
                                 let id = sector.sector_id
                                 return <div class="column is-1">
-                                    <button onClick={this.handleClick} value={id} class="button is-large is-success">{id}</button>
+                                    <button onClick={this.handleClick} value={id} class="button is-large is-danger is-outlined">{id}</button>
                                 </div>
                             })}
                         </div>
@@ -82,7 +125,7 @@ class History extends Component {
         if (value != null) {
             loadedContent =
                 <div className="columns">
-                    <div style={{margin: '0 auto'}}>
+                    <div style={{ margin: '0 auto' }}>
                         <DateRangePicker
                             onChange={this.onChange}
                             value={this.state.dates}
@@ -90,17 +133,43 @@ class History extends Component {
                     </div>
                 </div>
         }
+
+        if (historyLoaded === true) {
+            setContent =
+                <div className="columns">
+                    <div className="column">
+                        <ResponsiveContainer width='100%' aspect={4.0 / 3.0} height={250} >
+                            <LineChart data={history}
+                                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="name" />
+                                <YAxis dataKey="occupance" />
+                                <Tooltip />
+                                <Legend />
+                                <Line type="monotone" dataKey="occupance" stroke="#8884d8" />
+                            </LineChart>
+                        </ResponsiveContainer>
+
+                    </div>
+                </div>
+
+
+
+        }
+
         return <div className='page'>
-            <Navbar />
+            <Herosub title='History' />
+
             <div className="container">
                 <div className="content">
                     <section className="section">
-                        <h1 className="title is-size-1">Single sector</h1>
                         {content}
 
                         <hr />
 
                         {loadedContent}
+
+                        {setContent}
                     </section>
                 </div>
             </div>
